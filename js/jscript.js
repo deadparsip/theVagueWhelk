844828b4b8b51044727253f24000e9cc30873785
jonStevens <jon.stevens@team.pottermore.com>
2015-06-08 16:18:45 +0100

    tidies

--- js/jscript.js
+++ js/jscript.js
@@ -9,6 +9,7 @@ function Calippo() {
 		$next = $('a.next'),
 		$prev = $('a.prev'),
 		$boxes = $('.boxes'),
+		arrBoxes = $.makeArray($boxes),
 		$nav = $('nav'),
 		navUlTops  = 0,
 		$navUl = $('nav ul').show(),
@@ -17,9 +18,11 @@ function Calippo() {
 		_navOffsets = 120,
 		$currentNav = $nav.find('li').eq(0),
 		$currentBox = $boxes.eq(0),
+		currentNum = 0,
 		$nextBox = $boxes.eq(1),
 		$prevBox = $boxes.eq(0),		
-		that=this;
+		that=this,
+		classes=[];
 		var biscuitLid =  function() {
 			return ($navUlHeight - (navUlTops * -1))  == _navOffsets
 		}
@@ -59,13 +62,13 @@ function Calippo() {
 			$currentBox = $currentBox.next();
 			$nextBox = $currentBox.next().length ? $currentBox.next() : "";		
 			$currentNav = $currentNav.hasClass('selected') ? $currentNav.next().addClass('selected') : $currentNav.addClass('selected');
-			$currentNav.siblings().removeClass('selected');		
+			$currentNav.siblings().removeClass('selected');
+			window.localStorage.setItem('box' + loc, $currentBox.attr('class').split(" ")[2]);
             $prevBox.removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutLeftBig').on('animationend webkitAnimationEnd', function () {
                 window.location.hash = "";                
                 $prevBox.hide().removeClass('fadeOutLeftBig').off('animationend webkitAnimationEnd');
                 $currentBox.show().addClass('fadeInRightBig');				
-            });
-            window.localStorage.setItem('box' + loc, $currentBox.attr('class').split(" ")[2]);	
+            });            
         }
     }
 	
@@ -78,31 +81,37 @@ function Calippo() {
 			$currentNav=$currentNav.prev().length ? $currentNav.prev().addClass('selected') : $currentNav.removeClass('selected');
 			$currentNav.siblings().removeClass('selected');	
 			window.location.hash = "";		        
+			window.localStorage.setItem('box' + loc, $currentBox.attr('class').split(" ")[2]);
             $nextBox.removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutRightBig').on('animationend webkitAnimationEnd', function () {                						                
                 $nextBox.hide().removeClass('fadeOutRightBig').off('animationend webkitAnimationEnd');
                 $currentBox.show().addClass('fadeInLeftBig');
-            });
-            window.localStorage.setItem('box' + loc, $currentBox.attr('class').split(" ")[2]);	
+            });            
         }
     }
 	
 	
-    function getItem(e) {        
-		var $t = $(e.target),
-			item = $.fn.whelkit($t.attr('class'));
-		if ($t.hasClass('selected')) return;			
-        $currentBox.removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutLeftBig').on('animationend webkitAnimationEnd', function () {
-			$currentBox = $('.boxes.'+item);
-			window.localStorage.setItem('box' + loc, $currentBox.attr('class').split(" ")[2]);
-            $nextBox = $currentBox.next();
-            $prevBox = $currentBox.prev('article').length ? $currentBox.prev() : "";
-            $(this).hide().removeClass('fadeOutLeftBig').off('animationend webkitAnimationEnd');
-            $currentBox.show().addClass('fadeInLeftBig');				
-        });
-		$currentNav = $(e.target);
-		$currentNav.addClass('selected').siblings().removeClass('selected');		
+    function getItem(targetName) {        
+		currentNum = (classes.indexOf($.fn.whelkit(targetName)));
+		if (currentNum>-1) {
+			if ($currentBox.is(':visible')) {
+			$currentBox.removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutLeftBig').on('animationend webkitAnimationEnd',showNew);
+			}
+			else {
+				showNew();
+			}
+		}
     }
 	
+	function showNew(){
+		$currentBox = $boxes.eq(currentNum);
+		$nextBox = $currentBox.next();
+		$prevBox = $currentBox.prev('article').length ? $currentBox.prev() : "";
+		window.localStorage.setItem('box' + loc, $currentBox.attr('class').split(" ")[2]);			
+		$(this).hide().removeClass('fadeOutLeftBig').off('animationend webkitAnimationEnd');
+		$currentBox.show().addClass('fadeInLeftBig');				
+		$currentNav = $linkers.eq(currentNum);
+		$currentNav.addClass('selected').siblings().removeClass('selected');			
+	}
 	
 	(function caching () {
 		if (cacheDate !== "trouserclap") {
@@ -138,23 +147,17 @@ function Calippo() {
 			$($boxes[i]).addClass(j);
 			var link = '<li class="circle '+ j +'">'+ i + '</li>';			
 			$(link).appendTo($navUl);
+			classes.push(j);
 		});
 				
 		$navUlHeight = $navUl.height();				
 		$linkers = $navUl.find('.circle');
 		
-        var box = window.localStorage.getItem('box' + loc);
-		if (box !== null && $('.'+box).length) {
-            $currentBox = $('.'+box).show().addClass('fadeInRightBig visible');						
-			$navUl.find('.'+box).addClass('selected');		
-			$prevBox = $currentBox.prev('article').length ? $currentBox.prev() : "";
-			$nextBox = $currentBox.next();
-			if ($prevBox.length > 0) {
-				$prev.removeClass('opac');
-			}	
-			navUlTops  = ($('nav li.selected').position().top) * -1;
-			$navUl.css('top',navUlTops);
-			if (biscuitLid()) $next.addClass('opac');
+        var box = window.localStorage.getItem('box' + loc),
+			currentNum = classes.indexOf(box);
+		
+		if (box !== null && currentNum>-1) {
+			getItem(box)
         } 
 		else {
 			$currentBox.show();
@@ -164,28 +167,19 @@ function Calippo() {
         $prev.on('click', prevNav);
         $boxes.on("swiperight", prevItem);
         $boxes.on("swipeleft", nextItem);
-
-
-		$('.helper').show();
+		$nav.on('click', 'li', function (event) { getItem(event.target.className); });
+		$('.helper').show();		
 		setTimeout(function () {
 		   $helper.fadeOut(500);
-		}, 3000);			
-				
-		$nav.on('click', 'li', function (event) { getItem(event); });
+		}, 3000);				
 		
-
-		var show = false;
-
 		$linkers.each(function() {		
 			if($(this).position().top > 0) {
-				show=true;
+				$next.show();
+				$prev.show();
 				return
 			}			
-		});	
-		if (show==true) {			
-			$next.show();
-			$prev.show();
-		}		
+		});			
 
     }
 	
@@ -197,7 +191,7 @@ function Calippo() {
 
 (function($) {
 	$.fn.whelkit = function(text) {
-		return text.replace(/[^a-z,^A-Z]/g,'').replace('selected','').replace('circle','').replace('boxesanimated','').slice(0,30); 
+		return text.replace(/[^a-z,^A-Z]/g,'').replace('circle','').replace('boxesanimated','').slice(0,30); 
 	};
 }( jQuery ));
 
